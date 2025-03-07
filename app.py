1e6540d0105e2f513e68a4bf97a3bd7843b9c4b9
Vijayaraghavan2711 <vijayaraghavan.ilavarasan@cloudiq.io>
2025-03-07 17:26:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return "Hello, Azure!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
-o
+    app.run(host='0.0.0.0', port=8000)
+
 
